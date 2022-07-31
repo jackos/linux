bac10cabb7fcfc8f0b9b8f1cc0ade3a6a06d31a3
Jack Clayton <jackclayto@gmail.com>
2022-07-31 12:53:28 +0800

    4: register device

--- samples/rust/rust_vdev.rs
+++ samples/rust/rust_vdev.rs
@@ -1,21 +1,35 @@
 //! Virtual Device Module
 use kernel::prelude::*;
 
+use kernel::file::{File, Operations};
+use kernel::{miscdev, Module};
+
 module! {
     type: VDev,
     name: b"vdev",
     license: b"GPL",
 }
 
-struct VDev;
+struct VDev {
+    _dev: Pin<Box<miscdev::Registration<VDev>>>,
+}
 
-impl kernel::Module for VDev {
+#[vtable]
+impl Operations for VDev {
+    fn open(_context: &(), _file: &File) -> Result {
+        pr_info!("File was opened\n");
+        Ok(())
+    }
+}
+
+impl Module for VDev {
     fn init(_name: &'static CStr, _module: &'static ThisModule) -> Result<Self> {
         // Print a banner to make sure our moudle is working
         pr_info!("-----------------------\n");
         pr_info!("initialize vdev module!\n");
         pr_info!("watching for changes...\n");
         pr_info!("-----------------------\n");
-        Ok(VDev)
+        let reg = miscdev::Registration::new_pinned(fmt!("vdev"), ())?;
+        Ok(Self { _dev: reg })
     }
 }
